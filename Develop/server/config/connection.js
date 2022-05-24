7fdc054d30f45fb2de32a9e9b23d951d4725102e
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-23 18:23:02 -0600

    troubleshooting heroku

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernDB',
-{ 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 
 });
+
 
 module.exports = mongoose.connection;
 
