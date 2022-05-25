f134cfbc231dd34ca14b7c6600385546e1cd28c3
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-25 13:11:00 -0600

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/merndb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
