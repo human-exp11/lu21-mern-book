4c413cdd7ebc9344279de65a72c7498f493953e3
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-25 13:24:22 -0600

    troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/merndb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/3001', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
